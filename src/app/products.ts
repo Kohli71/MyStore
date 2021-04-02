8a31592b15e2a1557b31fc89e33cf0dad2abf92d
Kranthi0307 <you@example.com>
2021-04-02 18:45:29 +0530

    commit

---
+++ src/app/products.ts
@@ -0,0 +1,21 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Nokia X6',
+        price: 15999,
+        description: 'Specifications: 4GB RAM with 64GB Storage Camera: 13MP'
+    },
+
+    {
+        id: 2,
+        name: 'Redmi Note 7 Pro',
+        price: 11999,
+        description: 'Specifications: 4GB RAM with 64GB Storage Camera: 48MP'
+    },
+
+    {
+        id: 3,
+        name: 'Samsung M21',
+        price: 13999,
+    }
+]
